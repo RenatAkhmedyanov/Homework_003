37d9b73350b157072e438381437a47e4c7b85c75
Renat <renatakhmedyanov@gmail.com>
2022-10-22 13:34:36 +0400

    Add Task 19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+
+Console.Write("Введите положительное пятизначное число: ");
+int input = int.Parse(Console.ReadLine()!);
+
+if (input > 9999 && input < 100000)
+{
+    int firstDigit = input / 10000;
+    int secondDigit = (input / 1000) % 10;
+    int fourthDigit = (input / 10) % 10;
+    int fifthDigit = input % 10;
+    if (IsPalindrome(firstDigit, secondDigit, fourthDigit, fifthDigit))
+    {
+        Console.WriteLine("Число является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом.");
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод!");
+}
+
+bool IsPalindrome(int firstDigit, int secondDigit, int fourthDigit, int fifthDigit)
+{
+    return firstDigit == fifthDigit && secondDigit == fourthDigit;
+}
